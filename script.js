986d3dd30a8c14f5642dcba595db9f5af98a0b2f
nikhil-singh-iphtech <nikhil.singh@iphtechnologies.com>
2026-02-27 18:43:35 +0530

    done a lot of work

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+
+
+// const arr = [1, 2, , [4, ]];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (Array.isArray(arr[i])) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       result.push(arr[i][j]);
+//     }
+//   } else {
+//     result.push(arr[i]);
+//   }
+// }
+
+// console.log(result);
+// console.log(result.length);
+
+const arr = ["a", "b", "c", "d", "e", "f"];
+
+
+arr.splice(3, 1);
+arr.splice(2, 1); 
+arr.splice(0, 1); 
+
+console.log(arr);
+
+
+// const arr=[]
+
+// console.log(arr)
+
+// arr[1]=10;
+// arr[4]=15;
+
+// console.log(arr.length)
+// console.log(arr)
+
+// let temp=arr[1]
+// arr[1]=arr[4];
+// arr[4]=temp;
+
+// console.log(arr.length)
+// console.log(arr)
+
+
+
+
+
+
